63fad4bdb542438504036e7695eef92241d19296
sivanimarepalli <sivanimarepalli@gmail.com>
2026-07-17 11:34:40 +0530

    Day 7: Added Jinja loops and dynamic welcome message

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def helloMessage():
-    return render_template('index.html')
+    name="Sivani"
+    habits=["Drawing Practice","Coding Practice","Languages","Dashboard Project","Reading Practice"]
+    return render_template('index.html',habits=habits,name=name)
 
 if __name__=='__main__':
     app.run(debug=True)
