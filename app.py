05122fa594b6deaeee909df7487c8cdcea6899ab
sivanimarepalli <sivanimarepalli@gmail.com>
2026-07-19 10:10:26 +0530

    Day-9: Dynamic progress using Dictionaries

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ def helloMessage():
     name="Sivani"
     habits=["Drawing Practice","Coding Practice","Languages","Dashboard Project","Reading Practice"]
     habit_card_tsk=["Drawing","Languages","Dashboard","Coding","Excel"]
-    return render_template('index.html',habits=habits,name=name,habit_card_tsk=habit_card_tsk)
+    progress_tasks={"Dashboard":"40%",
+                   "Coding":"1/1",
+                   "Drawing":"1/1",
+                   "Excel":"0/1"}
+    return render_template('index.html',habits=habits,name=name,habit_card_tsk=habit_card_tsk,progress_tasks=progress_tasks)
 
 if __name__=='__main__':
     app.run(debug=True)
